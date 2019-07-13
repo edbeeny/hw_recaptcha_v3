5af103e11329d73dec06d83f1eeb747621c7c371
Ed Beeny <edbeeny@gmail.com>
2019-07-13 09:31:04 +0100

    Update Version Package

--- controller.php
+++ controller.php
@@ -11,7 +11,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'hw_recaptcha_v3';
     protected $appVersionRequired = '8.0';
-    protected $pkgVersion = '0.9.2';
+    protected $pkgVersion = '0.9.3';
 
     protected $logger;
 
