6320e86f07145f9dc8fd64b1f9f742504a062f97
Ed Beeny <edbeeny@gmail.com>
2019-07-19 11:05:10 +0100

    Update render.js

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,25 +1,16 @@
 function hwRecaptcha() {
-    $(".hwRecaptcha").each(function() {
+    $(".hwRecaptcha").each(function () {
         var el = $(this);
-        var clientId =  grecaptcha.render($(el).attr("id"), {
+        var clientId = grecaptcha.render($(el).attr("id"), {
             'sitekey': $(el).attr('data-sitekey'),
             'badge': $(el).attr('data-badge'),
             'theme': $(el).attr('data-theme'),
-            'size' : 'invisible',
-            'callback' : function(token) {
-                $(el).parent(".g-recaptcha-response").val(token);
-                $(el).parent().submit();
-            }
+            'size': 'invisible',
         });
-        grecaptcha.ready(function() {
+        grecaptcha.ready(function () {
             grecaptcha.execute(clientId, {
                 action: 'submit'
             })
         });
     });
-
-
-
-
-
-    }
+}
